f96b9cb47bc8189adb36c8e0af90556e4060843c
Simao Gomes Viana <xdevs23@outlook.com>
2016-10-05 15:41:30 +0200

    build: remove duplicated boot jars
    This filters out duplicated boot jars automatically, preventing dex2oatd errors etc.
    
    Change-Id: I04ba9150947d0945e98d6ef849f4e57508d076f4
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- core/product_config.mk
+++ core/product_config.mk
@@ -329,6 +329,15 @@ endef
 # Extra boot jars must be appended at the end after common boot jars.
 PRODUCT_BOOT_JARS += $(PRODUCT_BOOT_JARS_EXTRA)
 
+# Filter out duplicates
+define uniq__dx
+  $(eval seen :=)
+  $(foreach _,$1,$(if $(filter $_,${seen}),,$(eval seen += $_)))
+  ${seen}
+endef
+
+PRODUCT_BOOT_JARS := $(call uniq__dx,$(subst $(space), ,$(strip $(PRODUCT_BOOT_JARS))))
+
 PRODUCT_BOOT_JARS := $(call qualify-platform-jars,$(PRODUCT_BOOT_JARS))
 
 # b/191127295: force core-icu4j onto boot image. It comes from a non-updatable APEX jar, but has
