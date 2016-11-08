0be139d5ac7d76d3be0b5b6ce379eb8433eb05fc
Steve Kondik <steve@cyngn.com>
2016-11-08 12:51:59 -0800

    build: Require devices to opt-in for SDCLANG
     * Only devices which see a measureable benefit should use this
       since it requires more resources to build.
     * Set TARGET_USE_SDCLANG to true in your BoardConfig if your
       device benefits from it.
    
    Change-Id: I12ee20b0627ff0ca121d653cde482e5449a768cc
    Signed-off-by: Joe Maples <joe@frap129.org>

--- core/binary.mk
+++ core/binary.mk
@@ -414,9 +414,11 @@ ifeq ($(my_sdclang),true)
     endif
 endif
 ifeq ($(SDCLANG),true)
-    ifeq ($(my_sdclang),)
-        ifneq ($(my_sdclang_2),true)
-            my_sdclang := true
+    ifeq ($(TARGET_USE_SDCLANG),true)
+        ifeq ($(my_sdclang),)
+            ifneq ($(my_sdclang_2),true)
+                my_sdclang := true
+            endif
         endif
     endif
 endif
@@ -467,7 +469,6 @@ ifneq (,$(my_cpp_std_version))
    my_cpp_std_cppflags := -std=$(my_cpp_std_version)
 endif
 
-# arch-specific static libraries go first so that generic ones can depend on them
 my_static_libraries := $(LOCAL_STATIC_LIBRARIES_$($(my_prefix)$(LOCAL_2ND_ARCH_VAR_PREFIX)ARCH)) $(LOCAL_STATIC_LIBRARIES_$(my_32_64_bit_suffix)) $(my_static_libraries)
 my_whole_static_libraries := $(LOCAL_WHOLE_STATIC_LIBRARIES_$($(my_prefix)$(LOCAL_2ND_ARCH_VAR_PREFIX)ARCH)) $(LOCAL_WHOLE_STATIC_LIBRARIES_$(my_32_64_bit_suffix)) $(my_whole_static_libraries)
 my_header_libraries := $(LOCAL_HEADER_LIBRARIES_$($(my_prefix)$(LOCAL_2ND_ARCH_VAR_PREFIX)ARCH)) $(LOCAL_HEADER_LIBRARIES_$(my_32_64_bit_suffix)) $(my_header_libraries)
