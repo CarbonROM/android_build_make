5148a33f0f18ce6b7f3a8108283e0d21e9fddfe7
Adrian DC <radian.dc@gmail.com>
2017-07-24 14:04:34 +0200

    soong_config: Add TARGET_HAS_LEGACY_CAMERA_HAL1 variable
     * Soong product variable 'has_legacy_camera_hal1'
    
    Change-Id: I41da5ccfb11949c9251f21d80ded0ecf0f00166a
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -79,6 +79,7 @@ $(SOONG_VARIABLES): FORCE
 	echo '    "Safestack": $(if $(filter true,$(USE_SAFESTACK)),true,false),'; \
 	echo '    "EnableCFI": $(if $(filter false,$(ENABLE_CFI)),false,true),'; \
 	echo '    "Device_uses_hwc2": $(if $(filter true,$(TARGET_USES_HWC2)),true,false),'; \
+	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
 	echo '    "Override_rs_driver": "$(OVERRIDE_RS_DRIVER)",'; \
 	echo '    "Treble": $(if $(filter true,$(PRODUCT_FULL_TREBLE)),true,false),'; \
 	echo '    "Pdk": $(if $(filter true,$(TARGET_BUILD_PDK)),true,false),'; \
