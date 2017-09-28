CarbonROM/android_build_make

commit af75ff029fb791af5010638bbb9fa2f481bcea95

squashed commit of OmniROMs ota_from_target_files
Change-Id: I9dc5aa548a3f94a4719e7a6a645df01b6dd55b2c

tools/releasetools/ota_from_target_files.py
@@ -88,6 +88,25 @@ Usage:  ota_from_target_files [flags] input_target_files output_ota_package
   -e  (--extra_script)  <file>
       Insert the contents of file at the end of the update script.
 
+  --backup <boolean>
+      Enable or disable the execution of backuptool.sh.
+      Disabled by default.
+
+  --override_device <device>
+      Override device-specific asserts. Can be a comma-separated list.
+
+  --override_prop <boolean>
+      Override build.prop items with custom vendor init.
+      Enabled when TARGET_UNIFIED_DEVICE is defined in BoardConfig
+
+  --override_boot_partition <string>
+      Override the partition where the boot image is installed.
+      Used for devices with a staging partition (Asus Transformer).
+
+  --mount_by_label <boolean>
+      Force the OTA package to mount and format System by label
+      Can be enabled by defining TARGET_SETS_FSTAB. Defaults to false.
+
   -2  (--two_step)
       Generate a 'two-step' OTA package, where recovery is updated
       first, so that any changes made to the system partition are done
@@ -164,6 +183,11 @@ OPTIONS.extra_script = None
 OPTIONS.worker_threads = multiprocessing.cpu_count() // 2
 if OPTIONS.worker_threads == 0:
   OPTIONS.worker_threads = 1
+OPTIONS.backuptool = False
+OPTIONS.override_device = 'auto'
+OPTIONS.override_prop = False
+OPTIONS.override_boot_partition = ''
+OPTIONS.mount_by_label = False
 OPTIONS.two_step = False
 OPTIONS.no_signing = False
 OPTIONS.block_based = True
@@ -197,7 +221,10 @@ def SignOutput(temp_zip_name, output_zip_name):
 def AppendAssertions(script, info_dict, oem_dicts=None):
   oem_props = info_dict.get("oem_fingerprint_properties")
   if not oem_props:
-    device = GetBuildProp("ro.product.device", info_dict)
+    if OPTIONS.override_device == "auto":
+      device = GetBuildProp("ro.product.device", info_dict)
+    else:
+      device = OPTIONS.override_device
     script.AssertDevice(device)
   else:
     if not oem_dicts:
@@ -384,12 +411,18 @@ def WriteFullOTAPackage(input_zip, output_zip):
 
   target_fp = CalculateFingerprint(oem_props, oem_dicts and oem_dicts[0],
                                    OPTIONS.info_dict)
-  metadata = {
-      "post-build": target_fp,
-      "pre-device": GetOemProperty("ro.product.device", oem_props,
-                                   oem_dicts and oem_dicts[0],
-                                   OPTIONS.info_dict),
-      "post-timestamp": GetBuildProp("ro.build.date.utc", OPTIONS.info_dict),
+
+  if OPTIONS.override_prop:
+    metadata = {"post-timestamp": GetBuildProp("ro.build.date.utc",
+                                         OPTIONS.info_dict),
+                }
+  else:
+    metadata = {"post-build": target_fp,
+                "pre-device": GetOemProperty("ro.product.device", oem_props,
+                                             oem_dicts and oem_dicts[0],
+                                             OPTIONS.info_dict),
+                "post-timestamp": GetBuildProp("ro.build.date.utc",
+                                             OPTIONS.info_dict),
   }
 
   device_specific = common.DeviceSpecificParams(
@@ -458,10 +491,51 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     # Stage 3/3: Make changes.
     script.Comment("Stage 3/3")
 
+  script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
+
   # Dump fingerprints
   script.Print("Target: %s" % target_fp)
 
   device_specific.FullOTA_InstallBegin()
+
+  if OPTIONS.backuptool:
+    script.Mount("/system", OPTIONS.mount_by_label)
+    script.RunBackup("backup")
+    if not OPTIONS.mount_by_label:
+      script.Unmount("/system")
+
+  script.ShowProgress(0.5, 0)
+
+  if OPTIONS.wipe_user_data:
+    script.Print("Formatting /data")
+    script.FormatPartition("/data", OPTIONS.mount_by_label)
+
+  script.Print("                 ,....,                 ");
+  script.Print("           .,lx0XNWWWNKOd:.             ");
+  script.Print("        .:OWMMMMMMMMMMMMMMMXo.          ");
+  script.Print("      .oWMMMMMMMMMMMMMMMMMMMMM0,        ");
+  script.Print("     cWMMMMMMMMMMMMMMMMMMMMMMMMMO.      ");
+  script.Print("   .OMMMMMMMMMMMMMMMMMMMMMMMMMMWd.      ");
+  script.Print("  .KMMMMMMMMMKkdlllldkKMMMMMMMO.        ");
+  script.Print("  OMMMM`  x;.          .;xWMX;          ");
+  script.Print(" :MMMM  ..`               .;.           ");
+  script.Print(" KMMM  .o;                              ");
+  script.Print(".MMM  .0M;                              ");
+  script.Print("'MM` .KMMl                              ");
+  script.Print(".MN  xMMM0                              ");
+  script.Print(".NM ,MMMMM:                           ..");
+  script.Print(" dM :MMMMMW'                         .k ");
+  script.Print(" .N cMMMMMMW;                       'Xc ");
+  script.Print("  ,x;MMMMMMMMx.                   .oW0. ");
+  script.Print("   ,.NMMMMMMMMWx'                lNMK.  ");
+  script.Print("     ,NMMMMMMMMMMXx:'.     ..:o ,MMO.   ");
+  script.Print("      .kMMMMMMMMMMMMMMNXXXNMMW  NWl.    ");
+  script.Print("        ,OMMMMMMMMMMMMMMMMMMK  Xd.      ");
+  script.Print("          .oKMMMMMMMMMMMMMKc  :.        ");
+  script.Print("             .:okKNWWWNKdl'  '          ");
+  script.Print("                 ''...`',  +'           ");
+  script.Print("                                        ");
+  script.Print("                carbonrom.org           ");
 
   system_progress = 0.75
 
@@ -502,10 +576,17 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   common.CheckSize(boot_img.data, "boot.img", OPTIONS.info_dict)
   common.ZipWriteStr(output_zip, "boot.img", boot_img.data)
 
-  script.ShowProgress(0.05, 5)
-  script.WriteRawImage("/boot", "boot.img")
+  if OPTIONS.backuptool:
+    script.ShowProgress(0.2, 10)
+    script.RunBackup("restore")
 
   script.ShowProgress(0.2, 10)
+  script.Print("Flashing boot.img")
+  bootpartition = "/boot" if OPTIONS.override_boot_partition == "" else OPTIONS.override_boot_partition
+  script.WriteRawImage(bootpartition, "boot.img")
+
+  script.ShowProgress(0.1, 0)
+  script.Print("Enjoy Carbon ROM!");
   device_specific.FullOTA_InstallEnd()
 
   if OPTIONS.extra_script is not None:
@@ -604,13 +685,12 @@ def WriteBlockIncrementalOTAPackage(target_zip, source_zip, output_zip):
   if source_oem_props and target_oem_props:
     oem_dicts = _LoadOemDicts(script, recovery_mount_options)
 
-  metadata = {
-      "pre-device": GetOemProperty("ro.product.device", source_oem_props,
-                                   oem_dicts and oem_dicts[0],
-                                   OPTIONS.source_info_dict),
-      "ota-type": "BLOCK",
-  }
+  metadata = {}
+  if not OPTIONS.override_prop:
+    metadata["pre-device"] = GetOemProperty("ro.product.device", oem_props, oem_dict,
+                                   OPTIONS.source_info_dict)
 
+  metadata["ota-type"] = "BLOCK"
   HandleDowngradeMetadata(metadata)
 
   device_specific = common.DeviceSpecificParams(
@@ -623,12 +703,14 @@ def WriteBlockIncrementalOTAPackage(target_zip, source_zip, output_zip):
       metadata=metadata,
       info_dict=OPTIONS.source_info_dict)
 
-  source_fp = CalculateFingerprint(source_oem_props, oem_dicts and oem_dicts[0],
+  if not OPTIONS.override_prop:
+    source_fp = CalculateFingerprint(source_oem_props, oem_dicts and oem_dicts[0],
                                    OPTIONS.source_info_dict)
-  target_fp = CalculateFingerprint(target_oem_props, oem_dicts and oem_dicts[0],
+    target_fp = CalculateFingerprint(target_oem_props, oem_dicts and oem_dicts[0],
                                    OPTIONS.target_info_dict)
-  metadata["pre-build"] = source_fp
-  metadata["post-build"] = target_fp
+    metadata["pre-build"] = source_fp
+    metadata["post-build"] = target_fp
+
   metadata["pre-build-incremental"] = GetBuildProp(
       "ro.build.version.incremental", OPTIONS.source_info_dict)
   metadata["post-build-incremental"] = GetBuildProp(
@@ -840,6 +922,10 @@ else
     script.WriteRawImage("/boot", "boot.img")
     print("writing full boot image (forced by two-step mode)")
 
+  if OPTIONS.wipe_user_data:
+    script.Print("Erasing user data...")
+    script.FormatPartition("/data", OPTIONS.mount_by_label)
+
   if not OPTIONS.two_step:
     if updating_boot:
       if include_full_boot:
@@ -904,13 +990,18 @@ def WriteVerifyPackage(input_zip, output_zip):
 
   target_fp = CalculateFingerprint(oem_props, oem_dicts and oem_dicts[0],
                                    OPTIONS.info_dict)
-  metadata = {
-      "post-build": target_fp,
-      "pre-device": GetOemProperty("ro.product.device", oem_props,
-                                   oem_dicts and oem_dicts[0],
-                                   OPTIONS.info_dict),
-      "post-timestamp": GetBuildProp("ro.build.date.utc", OPTIONS.info_dict),
-  }
+  if OPTIONS.override_prop:
+    metadata = {
+        "post-timestamp": GetBuildProp("ro.build.date.utc", OPTIONS.info_dict),
+    }
+  else:
+    metadata = {
+        "post-build": target_fp,
+        "pre-device": GetOemProperty("ro.product.device", oem_props,
+                                     oem_dicts and oem_dicts[0],
+                                     OPTIONS.info_dict),
+        "post-timestamp": GetBuildProp("ro.build.date.utc", OPTIONS.info_dict),
+    }
 
   device_specific = common.DeviceSpecificParams(
       input_zip=input_zip,
@@ -1050,17 +1141,19 @@ def WriteABOTAPackageWithBrilloScript(target_file, output_file,
   if oem_props:
     oem_dicts = _LoadOemDicts(None)
 
-  metadata = {
-      "post-build": CalculateFingerprint(oem_props, oem_dicts and oem_dicts[0],
-                                         OPTIONS.info_dict),
-      "post-build-incremental" : GetBuildProp("ro.build.version.incremental",
-                                              OPTIONS.info_dict),
-      "pre-device": GetOemProperty("ro.product.device", oem_props,
+  metadata = {}
+  if not OPTIONS.override_prop:
+    metadata["pre-device"] = GetOemProperty("ro.product.device", oem_props,
                                    oem_dicts and oem_dicts[0],
                                    OPTIONS.info_dict),
-      "ota-required-cache": "0",
-      "ota-type": "AB",
-  }
+    metadata["post-build"] = CalculateFingerprint(oem_props, oem_dicts and oem_dicts[0],
+                                         OPTIONS.info_dict),
+    metadata["post-build-incremental"] = GetBuildProp("ro.build.version.incremental",
+                                              OPTIONS.info_dict),
+
+  metadata["post-timestamp"] = GetBuildProp("ro.build.date.utc", OPTIONS.info_dict)
+  metadata["ota-required-cache"] = "0"
+  metadata["ota-type"] = "AB"
 
   if source_file is not None:
     metadata["pre-build"] = CalculateFingerprint(oem_props,
@@ -1293,6 +1386,16 @@ def main(argv):
       else:
         raise ValueError("Cannot parse value %r for option %r - only "
                          "integers are allowed." % (a, o))
+    elif o in ("--backup"):
+      OPTIONS.backuptool = bool(a.lower() == 'true')
+    elif o in ("--override_device"):
+      OPTIONS.override_device = a
+    elif o in ("--override_prop"):
+      OPTIONS.override_prop = bool(a.lower() == 'true')
+    elif o in ("--override_boot_partition"):
+      OPTIONS.override_boot_partition = a
+    elif o in ("--mount_by_label"):
+      OPTIONS.mount_by_label = bool(a.lower() == 'true')
     elif o in ("-2", "--two_step"):
       OPTIONS.two_step = True
     elif o == "--no_signing":
@@ -1338,7 +1441,12 @@ def main(argv):
                                  "override_timestamp",
                                  "extra_script=",
                                  "worker_threads=",
+                                 "backup=",
+                                 "override_device=",
+                                 "override_prop=",
+                                 "override_boot_partition=",
                                  "two_step",
+                                 "mount_by_label=",
                                  "no_signing",
                                  "block",
                                  "binary=",

core/Makefile
@@ -288,6 +288,8 @@ endif
 			TARGET_CPU_ABI="$(TARGET_CPU_ABI)" \
 			TARGET_CPU_ABI2="$(TARGET_CPU_ABI2)" \
 			TARGET_AAPT_CHARACTERISTICS="$(TARGET_AAPT_CHARACTERISTICS)" \
+			TARGET_UNIFIED_DEVICE="$(TARGET_UNIFIED_DEVICE)" \
+			TARGET_SETS_FSTAB="$(TARGET_SETS_FSTAB)" \
 	        bash $(BUILDINFO_SH) >> $@
 ifndef property_overrides_split_enabled
 	$(hide) TARGET_BOOTLOADER_BOARD_NAME="$(TARGET_BOOTLOADER_BOARD_NAME)" \
@@ -2427,6 +2429,33 @@ INTERNAL_OTA_PACKAGE_TARGET := $(PRODUCT_OUT)/$(name).zip
 
 $(INTERNAL_OTA_PACKAGE_TARGET): KEY_CERT_PAIR := $(DEFAULT_KEY_CERT_PAIR)
 
+ifneq ($(CARBON_BUILD),)
+    $(INTERNAL_OTA_PACKAGE_TARGET): backuptool := true
+else
+     $(INTERNAL_OTA_PACKAGE_TARGET): backuptool := false
+endif
+
+ifeq ($(TARGET_OTA_ASSERT_DEVICE),)
+    $(INTERNAL_OTA_PACKAGE_TARGET): override_device := auto
+else
+    $(INTERNAL_OTA_PACKAGE_TARGET): override_device := $(TARGET_OTA_ASSERT_DEVICE)
+endif
+
+ifeq ($(TARGET_UNIFIED_DEVICE),)
+    $(INTERNAL_OTA_PACKAGE_TARGET): override_prop := false
+else
+    $(INTERNAL_OTA_PACKAGE_TARGET): override_prop := true
+endif
+
+ifneq ($(TARGET_SETS_FSTAB),)
+    $(INTERNAL_OTA_PACKAGE_TARGET): mount_by_label := true
+else
+    $(INTERNAL_OTA_PACKAGE_TARGET): mount_by_label := false
+    ifeq ($(TARGET_OTA_ASSERT_DEVICE),)
+        $(INTERNAL_OTA_PACKAGE_TARGET): override_device := $(TARGET_DEVICE)
+    endif
+endif
+
 $(INTERNAL_OTA_PACKAGE_TARGET): $(BUILT_TARGET_FILES_PACKAGE) \
 		build/tools/releasetools/ota_from_target_files
 	@echo "Package OTA: $@"
@@ -2436,7 +2465,12 @@ $(INTERNAL_OTA_PACKAGE_TARGET): $(BUILT_TARGET_FILES_PACKAGE) \
 	   --extracted_input_target_files $(patsubst %.zip,%,$(BUILT_TARGET_FILES_PACKAGE)) \
 	   -p $(HOST_OUT) \
 	   -k $(KEY_CERT_PAIR) \
+	   --backup=$(backuptool) \
+	   --override_device=$(override_device) \
+	   --override_prop=$(override_prop) \
+	   --mount_by_label=$(mount_by_label) \
 	   $(if $(OEM_OTA_CONFIG), -o $(OEM_OTA_CONFIG)) \
+	   $(TARGET_RELEASETOOL_OTA_FROM_TARGET_ADDITIONAL_ARGS) \
 	   $(BUILT_TARGET_FILES_PACKAGE) $@
 
 CARBON_CODENAME := LUX
@@ -2490,7 +2524,8 @@ measandwich: sandwich
 
 .PHONY: otapackage carbon
 otapackage: $(INTERNAL_OTA_PACKAGE_TARGET)
-carbon: otapackage
+carbon: CARBON_BUILD := true
+	otapackage
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(CARBON_TARGET_PACKAGE)
 	$(hide) $(MD5SUM) $(CARBON_TARGET_PACKAGE) > $(CARBON_TARGET_PACKAGE).md5sum
 	@echo ""

tools/buildinfo.sh
@@ -26,10 +26,7 @@ fi
 if [ -n "$AB_OTA_UPDATER" ] ; then
   echo "ro.build.ab_update=$AB_OTA_UPDATER"
 fi
-echo "ro.product.model=$PRODUCT_MODEL"
 echo "ro.product.brand=$PRODUCT_BRAND"
-echo "ro.product.name=$PRODUCT_NAME"
-echo "ro.product.device=$TARGET_DEVICE"
 
 # These values are deprecated, use "ro.product.cpu.abilist"
 # instead (see below).
@@ -52,9 +49,15 @@ echo "ro.wifi.channels=$PRODUCT_DEFAULT_WIFI_CHANNELS"
 echo "# ro.build.product is obsolete; use ro.product.device"
 echo "ro.build.product=$TARGET_DEVICE"
 
-echo "# Do not try to parse description, fingerprint, or thumbprint"
-echo "ro.build.description=$PRIVATE_BUILD_DESC"
-echo "ro.build.fingerprint=$BUILD_FINGERPRINT"
+if [ "$TARGET_UNIFIED_DEVICE" == "" ] ; then
+  echo "ro.product.model=$PRODUCT_MODEL"
+  echo "ro.product.name=$PRODUCT_NAME"
+  echo "ro.product.device=$TARGET_DEVICE"
+  echo "# Do not try to parse ro.build.description or .fingerprint"
+  echo "ro.build.description=$PRIVATE_BUILD_DESC"
+  echo "ro.build.fingerprint=$BUILD_FINGERPRINT"
+fi
+
 if [ -n "$BUILD_THUMBPRINT" ] ; then
   echo "ro.build.thumbprint=$BUILD_THUMBPRINT"
 fi

tools/releasetools/edify_generator.py
@@ -145,11 +145,13 @@ class EdifyGenerator(object):
 
   def AssertDevice(self, device):
     """Assert that the device identifier is the given string."""
-    cmd = ('getprop("ro.product.device") == "%s" || '
-           'abort("E%d: This package is for \\"%s\\" devices; '
-           'this is a \\"" + getprop("ro.product.device") + "\\".");') % (
-               device, common.ErrorCode.DEVICE_MISMATCH, device)
-    self.script.append(cmd)
+    cmd = ('(' +
+           ' || \0'.join(['getprop("ro.product.device") == "%s" || getprop("ro.build.product") == "%s"'
+                         % (i, i) for i in device.split(",")]) +
+           ') || abort(E%d: This package is for \\"%s\\" devices\n'
+           'this is a \\"" + getprop("ro.product.device") + "\\".");'
+           ) % (common.ErrorCode.DEVICE_MISMATCH, device)
+    self.script.append(self.WordWrap(cmd))
 
   def AssertSomeBootloader(self, *bootloaders):
     """Asert that the bootloader version is one of *bootloaders."""
@@ -158,6 +160,14 @@ class EdifyGenerator(object):
                          for b in bootloaders]) +
            ");")
     self.script.append(self.WordWrap(cmd))
+
+  def RunBackup(self, command):
+    self.script.append('package_extract_file("system/bin/backuptool.sh", "/system/bin/backuptool.sh");')
+    self.script.append('package_extract_file("system/bin/backuptool.functions", "/tmp/backuptool.functions");')
+    self.SetPermissions("/system/bin/backuptool.sh", 0, 0, 0755, None, None)
+    self.script.append(('run_program("/system/bin/backuptool.sh", "%s");' % command))
+    if command == "restore":
+      self.DeleteFiles(["/tmp/backuptool.functions"])
 
   def ShowProgress(self, frac, dur):
     """Update the progress bar, advancing it over 'frac' over the next
@@ -206,7 +216,7 @@ class EdifyGenerator(object):
                             amount,
                             common.ErrorCode.INSUFFICIENT_CACHE_SPACE))
 
-  def Mount(self, mount_point, mount_options_by_format=""):
+  def Mount(self, mount_point, mount_by_label = False, mount_options_by_format=""):
     """Mount the partition with the given mount_point.
       mount_options_by_format:
       [fs_type=option[,option]...[|fs_type=option[,option]...]...]
@@ -216,19 +226,25 @@ class EdifyGenerator(object):
     fstab = self.fstab
     if fstab:
       p = fstab[mount_point]
-      mount_dict = {}
-      if mount_options_by_format is not None:
-        for option in mount_options_by_format.split("|"):
-          if "=" in option:
-            key, value = option.split("=", 1)
-            mount_dict[key] = value
-      mount_flags = mount_dict.get(p.fs_type, "")
-      if p.context is not None:
-        mount_flags = p.context + ("," + mount_flags if mount_flags else "")
-      self.script.append('mount("%s", "%s", "%s", "%s", "%s");' % (
-          p.fs_type, common.PARTITION_TYPES[p.fs_type], p.device,
-          p.mount_point, mount_flags))
+      if mount_by_label:
+        self.script.append('run_program("/sbin/mount", "%s");' % (mount_point,))
+      else:
+        mount_dict = {}
+        if mount_options_by_format is not None:
+          for option in mount_options_by_format.split("|"):
+            if "=" in option:
+              key, value = option.split("=", 1)
+              mount_dict[key] = value
+        self.script.append('mount("%s", "%s", "%s", "%s", "%s");' %
+                         (p.fs_type, common.PARTITION_TYPES[p.fs_type],
+                          p.device, p.mount_point, mount_dict.get(p.fs_type, "")))
       self.mounts.add(p.mount_point)
+
+  def Unmount(self, mount_point):
+    """Unmount the partiiton with the given mount_point."""
+    if mount_point in self.mounts:
+      self.mounts.remove(mount_point)
+      self.script.append('unmount("%s");' % (mount_point,))
 
   def UnpackPackageDir(self, src, dst):
     """Unpack a given directory from the OTA package into the given
@@ -257,16 +273,21 @@ class EdifyGenerator(object):
         '"%s") || abort("E%d: Failed to tune partition %s");' % (
             p.device, common.ErrorCode.TUNE_PARTITION_FAILURE, partition))
 
-  def FormatPartition(self, partition):
+  def FormatPartition(self, partition, mount_by_label = False):
     """Format the given partition, specified by its mount point (eg,
     "/system")."""
 
     fstab = self.fstab
     if fstab:
       p = fstab[partition]
-      self.script.append('format("%s", "%s", "%s", "%s", "%s");' %
-                         (p.fs_type, common.PARTITION_TYPES[p.fs_type],
-                          p.device, p.length, p.mount_point))
+      if mount_by_label:
+        if not p.mount_point in self.mounts:
+          self.script.mount(p.mount_point)
+        self.script.append('run_program("/sbin/rm", "-rf", "%s");' % (p.mount_point,))
+      else:
+        self.script.append('format("%s", "%s", "%s", "%s", "%s");' %
+                           (p.fs_type, common.PARTITION_TYPES[p.fs_type],
+                            p.device, p.length, p.mount_point))
 
   def WipeBlockDevice(self, partition):
     if partition not in ("/system", "/vendor"):
@@ -309,6 +330,11 @@ class EdifyGenerator(object):
         else:
           self.script.append(
               'package_extract_file("%(fn)s", "%(device)s");' % args)
+      elif partition_type == "BML":
+          self.script.append(
+            ('assert(package_extract_file("%(fn)s", "/tmp/%(device)s.img"),\n'
+             '       write_raw_image("/tmp/%(device)s.img", "%(device)s"),\n'
+             '       delete("/tmp/%(device)s.img"));') % args)
       else:
         raise ValueError(
             "don't know how to write \"%s\" partitions" % p.fs_type)

commit 9478af3675e51958d8e07b55004655b564b186d8

OTA: Fix wrong pre-device assignment introduced by af75ff029fb791af5010638bbb9fa2f481bcea95
Change-Id: I1ebedb4633070d48ff7fd988ef1f7cfdbeeda906

tools/releasetools/ota_from_target_files.py
@@ -703,7 +703,8 @@ def WriteBlockIncrementalOTAPackage(target_zip, source_zip, output_zip):
 
   metadata = {}
   if not OPTIONS.override_prop:
-    metadata["pre-device"] = GetOemProperty("ro.product.device", oem_props, oem_dict,
+    metadata["pre-device"] = GetOemProperty("ro.product.device", source_oem_props,
+                                   oem_dicts and oem_dicts[0],
                                    OPTIONS.source_info_dict)
 
   metadata["ota-type"] = "BLOCK"