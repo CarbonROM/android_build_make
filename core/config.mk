erfanoabdi/build_make

config: Include custom boardconfig a bit later
- We're trying to export MAKE_PREBUILT to soong
- Let's include it right before soong_config.mk is included

Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

core/config.mk
@@ -234,11 +234,6 @@ FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 # be device and hardware independent.
 $(call project-set-path-variant,ril,TARGET_RIL_VARIANT,hardware/ril)
 
--include vendor/extra/BoardConfigExtra.mk
-ifneq ($(POTATO_BUILD),)
-include vendor/potato/config/BoardConfigPotato.mk
-endif
-
 # The build system exposes several variables for where to find the kernel
 # headers:
 #   TARGET_DEVICE_KERNEL_HEADERS is automatically created for the current
@@ -1185,6 +1180,11 @@ dont_bother_goals := out \
     recoveryimage-nodeps \
     vbmetaimage-nodeps \
     product-graph dump-products
+
+-include vendor/extra/BoardConfigExtra.mk
+ifneq ($(POTATO_BUILD),)
+include vendor/potato/config/BoardConfigPotato.mk
+endif
 
 ifeq ($(CALLED_FROM_SETUP),true)
 include $(BUILD_SYSTEM)/ninja_config.mk

CarbonROM/android_build_make

Change-Id: I93308c35f5d3112da51fa944b7ed193ccff8e3e8

core/config.mk
@@ -234,10 +234,6 @@ FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 # be device and hardware independent.
 $(call project-set-path-variant,recovery,RECOVERY_VARIANT,bootable/recovery)
 
-ifneq ($(CARBON_BUILD),)
-include vendor/carbon/config/BoardConfigCarbon.mk
-endif
-
 # The build system exposes several variables for where to find the kernel
 # headers:
 #   TARGET_DEVICE_KERNEL_HEADERS is automatically created for the current
@@ -1183,6 +1179,7 @@ ifneq ($(CARBON_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
 $(eval include device/carbon/sepolicy/common/sepolicy.mk)
+include vendor/carbon/config/BoardConfigCarbon.mk
 endif
 
 # Include any vendor specific config.mk file